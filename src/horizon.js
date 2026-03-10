db74aaa503962a18109c2eeb7d308403be435f46
Ajax-16 <davidbernardezluque7618@gmail.com>
2026-03-10 19:32:48 +0100

    v0.3.4

--- src/horizon.js
+++ src/horizon.js
@@ -6,7 +6,7 @@ import bodyParser from "body-parser"
 export const HorizonJS = (config) => {
     new HorizonConfig(config);
     const app = express();
-    
+
     app.use(bodyParser.json({ limit: '50mb' }));
     app.use(bodyParser.urlencoded({ extended: true, limit: '50mb', parameterLimit: 100000 }));
     app.use(cors());
